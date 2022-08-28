4ee5d5313ae779de6a53dc408f1b7ed23d510f92
MrDragoRozum <roozumaleks@gmail.com>
2022-08-28 11:47:52 +0300

    Созданы два экрана

---
+++ app/src/main/java/com/example/todolist/AddNoteActivity.java
@@ -0,0 +1,63 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    private EditText editTextTextNote;
+    private RadioButton radioButtonLow;
+    private RadioButton radioButtonMedium;
+    private Button buttonSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        initViews();
+
+        buttonSave.setOnClickListener(l -> saveNote());
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, AddNoteActivity.class);
+    }
+
+    private void saveNote() {
+        String text = editTextTextNote.getText().toString().trim();
+        if(!text.isEmpty()) {
+            int priority = getPriority();
+        } else {
+            Toast.makeText(this,
+                    R.string.warning,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private int getPriority() {
+        int priority;
+
+        if(radioButtonLow.isChecked()) {
+            priority = 0;
+        } else if(radioButtonMedium.isChecked()) {
+            priority = 1;
+        } else {
+            priority = 2;
+        }
+        return priority;
+    }
+
+    private void initViews() {
+        editTextTextNote = findViewById(R.id.editTextTextNote);
+        radioButtonLow = findViewById(R.id.radioButtonLow);
+        radioButtonMedium = findViewById(R.id.radioButtonMedium);
+        buttonSave = findViewById(R.id.buttonSave);
+    }
+}
---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayoutNotes;
+    private FloatingActionButton buttonAddNote;
+
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+
+        Random random = new Random();
+        for(int i = 0; i < 20; i++) {
+            Note note = new Note(i, "Note: " + i, random.nextInt(3));
+            notes.add(note);
+        }
+        showNotes();
+
+        buttonAddNote.setOnClickListener(l -> {
+            Intent intent = AddNoteActivity.newIntent(this);
+            startActivity(intent);
+        });
+    }
+
+    private void initViews() {
+        linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
+    }
+
+    private void showNotes() {
+        for (Note note: notes) {
+            View view = getLayoutInflater().inflate(R.layout.note_item,
+                    linearLayoutNotes,
+                    false);
+            linearLayoutNotes.addView(view);
+            TextView textView = view.findViewById(R.id.textViewNote);
+            textView.setText(note.getText());
+
+            int colorResId;
+            switch (note.getPriority()) {
+                case 0:
+                    colorResId = android.R.color.holo_green_light;
+                    break;
+                case 1:
+                    colorResId = android.R.color.holo_orange_light;
+                    break;
+                default:
+                    colorResId = android.R.color.holo_red_light;
+                    break;
+            }
+            int color = ContextCompat.getColor(this, colorResId);
+            textView.setBackgroundColor(color);
+        }
+    }
+}
